a1811bf512b20e71e87a70c84d23f81f78a09bad
Steven Moreland <smoreland@google.com>
2017-06-05 17:25:55 -0700

    Fix error on user builds with future compiler.
    This resolves "Generated code has unused lambda captures"
    on future compiler versions.
    
    Test: build hidl modules on user and userdebug variants
    Test: boot internal marlin and pass hidl_test
    Change-Id: I1b68701646cb4cd860860fb43a3cf276b34e1e2c
    Fixes: 37908609

--- generateCpp.cpp
+++ generateCpp.cpp
@@ -717,10 +717,11 @@ status_t AST::generatePassthroughMethod(Formatter &out,
     out << "auto _hidl_return = ";
 
     if (method->isOneway()) {
-        out << "addOnewayTask([mImpl = this->mImpl, "
-               "mEnableInstrumentation = this->mEnableInstrumentation, "
-               "mInstrumentationCallbacks = this->mInstrumentationCallbacks, "
-               "&_hidl_error";
+        out << "addOnewayTask([mImpl = this->mImpl\n"
+            << "#ifdef __ANDROID_DEBUGGABLE__\n"
+               ", mEnableInstrumentation = this->mEnableInstrumentation, "
+               "mInstrumentationCallbacks = this->mInstrumentationCallbacks\n"
+            << "#endif // __ANDROID_DEBUGGABLE__\n";
         for (const auto &arg : method->args()) {
             out << ", "
                 << (arg->type().isInterface() ? "_hidl_wrapped_" : "")
